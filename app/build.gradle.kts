e4d68d9f1f96267c5028bace3e3c759366ecf8c5
wkbin <wangkebin1997@gmail.com>
2025-12-30 20:16:13 +0800

    version 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "top.wkbin.verydark"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
